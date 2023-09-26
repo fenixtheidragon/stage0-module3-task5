f8a4bd2c5903733b05499ec9612178edb2385e8d
fenix <fenixtheidragon@gmail.com>
2023-09-26 10:19:45 +0300

    little fun with arithmetic operations

--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public void convert(float minutes) {
+        int minutesInt = (int) (minutes * 100) / 100;
+        int seconds = (int) (minutesInt * 60 + (minutes - minutesInt) * 100);
+        System.out.println(seconds);
     }
 }
